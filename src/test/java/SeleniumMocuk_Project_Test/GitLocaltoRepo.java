b138426c76e5dcca82870848c6ea8ae3deab0865
parameswari-m91 <parameswari1991>
2025-08-25 19:18:38 +0530

    second changes

---
+++ src/test/java/SeleniumMocuk_Project_Test/GitLocaltoRepo.java
@@ -0,0 +1,11 @@
+package SeleniumMocuk_Project_Test;
+
+public class GitLocaltoRepo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Changes code on local and push changes from local to git repository");
+
+	}
+
+}
